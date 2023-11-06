b7f9cc73d30a42a1af61fd118cbb3c764bfdd87f
SCS <shettysachith47@gmail.com>
2023-11-06 18:35:39 +0530

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -17,21 +17,30 @@ fn wrap_text(input_text: &str, term_width: usize) -> Vec<String> {
 }
 
 fn print_img(img: Vec<&str>, quote: &str) {
-    let width = 50;
-    let imlen = img.len();
-    let wquote = wrap_text(quote, width);
-    let qlen = wquote.len();
-    let start = (imlen - qlen) / 2 as usize;
+    let term_width = terminal::size().unwrap().0 as usize;
+    let width = term_width.checked_sub(img[0].len() + 6);
+    match width {
+        Some(value) => {
+            let imlen = img.len();
+            let wquote = wrap_text(quote, value);
+            let qlen = wquote.len();
+            let start = (imlen - qlen) / 2 as usize;
 
-    for i in 0..start {
-        println!("{}", img[i]);
-    }
-    for i in start..(start + qlen) {
-        println!("{}  {}", img[i], wquote[i - start].clone().red());
-    }
+            for i in 0..start {
+                println!("{}", img[i]);
+            }
+            for i in start..(start + qlen) {
+                println!("{}  {}", img[i], wquote[i - start].clone().red());
+            }
 
-    for i in (start + qlen)..imlen {
-        println!("{}", img[i]);
+            for i in (start + qlen)..imlen {
+                println!("{}", img[i]);
+            }
+        }
+        _ => {
+            println!("{}", fill_text(quote, term_width).red());
+            println!("< Terminal width too small, try expanding window or reducing font size >");
+        }
     }
 }
 
