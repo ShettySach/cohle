1fda2f4198dd61239cd433bd5dfc135a8bf0c097
SCS <shettysachith47@gmail.com>
2023-11-06 10:19:54 +0530

    new file:   img.txt 	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,36 @@ use crossterm::style::Stylize;
 use crossterm::terminal;
 use rand::Rng;
 use std::fs;
-use textwrap::fill;
+use textwrap::{fill, wrap};
 
-fn wrap_text(input_text: &str, term_width: usize) -> String {
-    let wrapped_text = fill(input_text, term_width);
-    wrapped_text
+fn fill_text(input_text: &str, term_width: usize) -> String {
+    let filled_text = fill(input_text, term_width);
+    filled_text
+}
+
+fn wrap_text(input_text: &str, term_width: usize) -> Vec<String> {
+    let wrapped_text = wrap(input_text, term_width);
+    let vec_str: Vec<String> = wrapped_text.iter().map(|cow| cow.to_string()).collect();
+    vec_str
+}
+
+fn print_img(img: Vec<&str>, quote: &str) {
+    let width = 50;
+    let imlen = img.len();
+    let wquote = wrap_text(quote, width);
+    let qlen = wquote.len();
+    let start = (imlen - qlen) / 2 as usize;
+
+    for i in 0..start {
+        println!("{}", img[i]);
+    }
+    for i in start..(start + qlen) {
+        println!("{}  {}", img[i], wquote[i - start].clone().red());
+    }
+
+    for i in (start + qlen)..imlen {
+        println!("{}", img[i]);
+    }
 }
 
 fn main() {
@@ -25,10 +50,18 @@ fn main() {
                 .visible_alias("l")
                 .about("Lists all the quotes along with their indices"),
         )
+        .subcommand(
+            Command::new("image")
+                .visible_alias("i")
+                .about("Print with image"),
+        )
         .get_matches();
 
     let content = fs::read_to_string("quotes.txt").expect("Error in reading quotes.txt");
     let quotes = content.lines().collect::<Vec<&str>>();
+
+    let content = fs::read_to_string("img.txt").expect("Error in reading quotes.txt");
+    let imgs = content.lines().collect::<Vec<&str>>();
 
     let term_width = terminal::size().unwrap().0 as usize;
 
@@ -42,16 +75,28 @@ fn main() {
                 "\n Use 'cohle n' to print the nth quote or use 'cohle' to print a random quote."
             );
         }
+        Some("image") => {
+            if res.contains_id("index") {
+                let ind: usize = *res
+                    .get_one("index")
+                    .expect("Index must have value from 0 to n");
+                print_img(imgs, quotes[ind]);
+            } else {
+                let mut rng = rand::thread_rng();
+                let ind: usize = rng.gen_range(0..quotes.len());
+                print_img(imgs, quotes[ind]);
+            }
+        }
         _ => {
             if res.contains_id("index") {
                 let ind: usize = *res
                     .get_one("index")
                     .expect("Index must have value from 0 to n");
-                println!("{}", wrap_text(quotes[ind], term_width).red());
+                println!("{}", fill_text(quotes[ind], term_width).red());
             } else {
                 let mut rng = rand::thread_rng();
                 let ind: usize = rng.gen_range(0..quotes.len());
-                println!("{}", wrap_text(quotes[ind], term_width).red());
+                println!("{}", fill_text(quotes[ind], term_width).red());
             }
         }
     }
